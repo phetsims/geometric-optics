f18a9bd071bec91abc02a2dbb779646b8296c956
Martin Veillette <martin.veillette@gmail.com>
2021-05-10 09:08:29 -0400

    add constrained second point

--- js/common/view/SourceObjectNode.js
+++ js/common/view/SourceObjectNode.js
@@ -9,39 +9,66 @@ import Node from '../../../../scenery/js/nodes/Node.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import geometricOptics from '../../geometricOptics.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
+import Circle from '../../../../scenery/js/nodes/Circle.js';
+import Vector2Property from '../../../../dot/js/Vector2Property.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+
+const OVERALL_SCALE_FACTOR = 0.5;
+const OFFSET_VECTOR = new Vector2( 0.15, -0.18 );
 
 class SourceObjectNode extends Node {
 
   /**
    * @param {SourceObject} sourceObject
+   * @param {Property.<boolean>} visibleMovablePointProperty
    * @param {ModelViewTransform2} modelViewTransform
    * @param {Tandem} tandem
-   */
-  constructor( sourceObject, modelViewTransform, tandem ) {
+   * */
+  constructor( sourceObject, visibleMovablePointProperty, modelViewTransform, tandem ) {
     assert && assert( tandem instanceof Tandem, 'invalid tandem' );
-
     super();
+    const sourceObjectImage = new Image( sourceObject.typeProperty.value.source, { scale: OVERALL_SCALE_FACTOR } );
 
-    const dragListener = new DragListener(
-      {
-        positionProperty: sourceObject.positionProperty,
-        transform: modelViewTransform
-      } );
+    const leftTopModelPositionProperty = new Vector2Property( sourceObject.positionProperty.value.minus( OFFSET_VECTOR ) );
 
-    const object = new Image( sourceObject.typeProperty.value.source );
+    const sourceObjectDragListener = new DragListener( {
+      positionProperty: leftTopModelPositionProperty,
+      transform: modelViewTransform
+    } );
+
+
+    sourceObjectImage.addInputListener( sourceObjectDragListener );
+
+    leftTopModelPositionProperty.link( position => {
+      const offsetPosition = position.plus( OFFSET_VECTOR );
+      sourceObject.setMovablePoint( offsetPosition.plusXY( 0, sourceObject.getVerticalOffset() ) );
+      sourceObject.positionProperty.value = offsetPosition;
+      sourceObjectImage.leftTop = modelViewTransform.modelToViewPosition( position );
+    } );
+
+    const movablePoint = new Circle( 10, { fill: 'yellow' } );
+
+    const movablePointDragListener = new DragListener( {
+      positionProperty: sourceObject.movablePositionProperty,
+      transform: modelViewTransform
+    } );
+
+    movablePoint.addInputListener( movablePointDragListener );
 
     sourceObject.typeProperty.link( type => {
-      object.image = type.source;
-    } );
-    object.scale( 0.5 );
-
-    object.addInputListener( dragListener );
-
-    sourceObject.positionProperty.link( position => {
-      object.translation = modelViewTransform.modelToViewPosition( position ).plusXY( -30, -40 );
+      sourceObjectImage.image = type.source;
     } );
 
-    this.addChild( object );
+    sourceObject.movablePositionProperty.link( position => {
+      const freeVerticalOffset = position.y - sourceObject.positionProperty.value.y;
+      sourceObject.clampVerticalOffset( freeVerticalOffset );
+      movablePoint.center = modelViewTransform.modelToViewPosition( sourceObject.getMovablePosition() );
+    } );
+
+    visibleMovablePointProperty.linkAttribute( movablePoint, 'visible' );
+
+    this.addChild( sourceObjectImage );
+    this.addChild( movablePoint );
   }
 
 }
