c0262eef763ae20ddd524779ace32be38382b38e
schang1 <schang1@swarthmore.edu>
2021-06-04 14:33:22 -0600

    added documentation, abstracted the setting of the guide rectangle position (see #7)
    Signed-off-by: schang1 <schang1@swarthmore.edu>

--- js/lens/view/GuideNode.js
+++ js/lens/view/GuideNode.js
@@ -11,6 +11,7 @@ import Node from '../../../../scenery/js/nodes/Node.js';
 import geometricOptics from '../../geometricOptics.js';
 import GeometricOpticsConstants from '../../common/GeometricOpticsConstants.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 
 const GUIDE_FULCRUM_RADIUS = GeometricOpticsConstants.GUIDE_FULCRUM_RADIUS;
 const GUIDE_WIDTH = GeometricOpticsConstants.GUIDE_WIDTH;
@@ -36,31 +37,47 @@ class GuideNode extends Node {
     const viewGuideWidth = modelViewTransform.modelToViewDeltaX( GUIDE_WIDTH );
     const viewGuideHeight = modelViewTransform.modelToViewDeltaX( GUIDE_HEIGHT );
 
+    // create fulcrum circle
     const fulcrumCircle = new Circle( GUIDE_FULCRUM_RADIUS, options );
+
+    // create guide rectangle poiting to the object
     const leftGuideRectangle = new Rectangle( fulcrumCircle.x, fulcrumCircle.y - viewGuideHeight / 2, viewGuideWidth, viewGuideHeight, options );
 
+    /**
+     * set the position of the guide rectangle such that its right end center is on the fulcrum point.
+     * @param {Vector2} viewFulcrumPosition
+     * @param {number} angle
+     */
+    const setGuideRectanglePosition = ( viewFulcrumPosition, angle ) => {
+      leftGuideRectangle.center = Vector2.createPolar( -viewGuideWidth / 2, -angle ).plus( viewFulcrumPosition );
+    };
+
+    // update the position of the fulcrum
     guide.fulcrumPositionProperty.link( position => {
-      const viewPosition = modelViewTransform.modelToViewPosition( position );
-      fulcrumCircle.center = viewPosition;
+      const viewFulcrumPosition = modelViewTransform.modelToViewPosition( position );
+      fulcrumCircle.center = viewFulcrumPosition;
 
-
-      leftGuideRectangle.center = viewPosition.plusXY( -viewGuideWidth / 2 * Math.cos( guide.rotationAngleProperty.value ),
-        viewGuideWidth / 2 * Math.sin( guide.rotationAngleProperty.value ) );
+      // position the rectangle
+      setGuideRectanglePosition( viewFulcrumPosition, guide.rotationAngleProperty.value );
     } );
 
+    // rotate the guide
     guide.rotationAngleProperty.link( ( angle, oldAngle ) => {
+
+      // for first angle
       if ( oldAngle === null ) {
         oldAngle = 0;
       }
-      const fulcrumPosition = modelViewTransform.modelToViewPosition( guide.fulcrumPositionProperty.value );
-      leftGuideRectangle.rotateAround( fulcrumPosition, -angle + oldAngle );
+
+      // rotate the guide
+      const viewFulcrumPosition = modelViewTransform.modelToViewPosition( guide.fulcrumPositionProperty.value );
+      leftGuideRectangle.rotateAround( viewFulcrumPosition, -angle + oldAngle );
 
       // position of the rectangle guide
-      leftGuideRectangle.center = fulcrumPosition.plusXY( -viewGuideWidth / 2 * Math.cos( angle ),
-        viewGuideWidth / 2 * Math.sin( angle ) );
+      setGuideRectanglePosition( viewFulcrumPosition, angle );
     } );
 
-
+    // add to scene graph
     this.addChild( leftGuideRectangle );
     this.addChild( fulcrumCircle );
   }
