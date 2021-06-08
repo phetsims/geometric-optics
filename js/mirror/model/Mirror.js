32f4b85b775bfa7bd784b64b6f6a43758f750e2c
Martin Veillette <martin.veillette@gmail.com>
2021-06-08 13:49:08 -0400

    address uniformity of mirrors (see #15)
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/mirror/model/Mirror.js
+++ js/mirror/model/Mirror.js
@@ -8,6 +8,7 @@
  */
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 import Shape from '../../../../kite/js/Shape.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
@@ -56,6 +57,7 @@ class Mirror extends Optic {
   /**
    * Returns the shape of a mirror.
    * The center point of the mirror is 'position'
+   *
    * @param {Vector2} position
    * @param {number} radius
    * @param {number} diameter
@@ -67,27 +69,41 @@ class Mirror extends Optic {
   getFillAndOutlineShapes( position, radius, diameter, curve, options ) {
 
     options = merge( {
-      thickness: 0.05 // thickness of mirror
+      thickness: 0.05 // horizontal separation between the two edges of the surfaces at the middle part
     }, options );
+
+    //TODO: abstract some of the shapes
 
     // convenience variables
     const halfHeight = diameter / 2;
+
+    // half of the width of the shape along the x -axis
     const halfWidth = radius - Math.sqrt( radius * radius - halfHeight * halfHeight );
-    const verticalOffset = 2 * options.thickness * halfWidth / halfHeight;
+
+    // vector offset between the two corners of the shape
+    const angle = Math.atan( halfHeight / radius );
+    const sign = ( curve === Optic.Curve.CONCAVE ) ? +1 : -1;
+    const offsetTopVector = Vector2.createPolar( options.thickness, angle * sign );
+    const offsetBottomVector = Vector2.createPolar( options.thickness, -angle * sign );
 
     const shapes = {};
 
     if ( this.isConvex( curve ) ) {
 
+      // top left of the shape
       const top = position.plusXY( halfWidth, halfHeight );
+
+      // bottom left of the shape
       const bottom = position.plusXY( halfWidth, -halfHeight );
+
+      // control point - the shape will not go through this point
       const left = position.plusXY( -halfWidth, 0 );
 
       shapes.fillShape = new Shape()
         .moveToPoint( top )
-        .quadraticCurveToPoint( left, bottom )
-        .lineToPoint( bottom.plusXY( options.thickness, verticalOffset ) )
-        .quadraticCurveToPoint( left.plusXY( options.thickness, 0 ), top.plusXY( options.thickness, -verticalOffset ) )
+        .quadraticCurveToPoint( left, bottom ) //
+        .lineToPoint( bottom.plus( offsetBottomVector ) )
+        .quadraticCurveToPoint( left.plusXY( options.thickness, 0 ), top.plus( offsetTopVector ) )
         .close();
 
       shapes.outlineShape = new Shape()
@@ -97,16 +113,23 @@ class Mirror extends Optic {
 
     }
     else {
+      // concave shape
 
+      // position of the shape
       const topLeft = position.plusXY( -halfWidth, halfHeight );
       const bottomLeft = position.plusXY( -halfWidth, -halfHeight );
+      const bottomRight = bottomLeft.plus( offsetBottomVector );
+      const topRight = topLeft.plus( offsetTopVector );
+
+      // control points
       const midLeft = position.plusXY( halfWidth, 0 );
+      const midRight = midLeft.plusXY( options.thickness, 0 );
 
       shapes.fillShape = new Shape()
         .moveToPoint( topLeft )
         .quadraticCurveToPoint( midLeft, bottomLeft )
-        .lineToPoint( bottomLeft.plusXY( options.thickness, -verticalOffset ) )
-        .quadraticCurveToPoint( midLeft.plusXY( options.thickness, 0 ), topLeft.plusXY( options.thickness, verticalOffset ) )
+        .lineToPoint( bottomRight )
+        .quadraticCurveToPoint( midRight, topRight )
         .close();
 
       shapes.outlineShape = new Shape()
