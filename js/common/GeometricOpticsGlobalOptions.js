08d03e6e2da79f24a032ab371af029a611129857
phet-dev <phetadmin@gmail.com>
2021-10-26 03:16:53 -0600

    update copyright dates from daily grunt work

--- js/common/GeometricOpticsGlobalOptions.js
+++ js/common/GeometricOpticsGlobalOptions.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2021, University of Colorado Boulder
 
 /**
  * GeometricOpticsGlobalOptions defines the global options for this simulation, accessed via PhET > Options.
