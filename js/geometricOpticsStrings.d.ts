a4542b5c369e8d0aeea82ad4ec0ebc12436368c8
pixelzoom <cmalley@pixelzoom.com>
2021-11-01 19:39:35 -0600

    TODO https://github.com/phetsims/joist/issues/764

--- js/geometricOpticsStrings.d.ts
+++ js/geometricOpticsStrings.d.ts
@@ -1,5 +1,7 @@
 // Copyright 2021, University of Colorado Boulder
 
+//TODO https://github.com/phetsims/joist/issues/764 what pattern should be used here?
+
 type t = {
   [ key: string ]: string,
   'geometric-optics': {
