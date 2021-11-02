1a075d7468f7e22394a693c5b36ffb6322ec1f82
pixelzoom <cmalley@pixelzoom.com>
2021-11-02 14:01:05 -0600

    TypeScript conversion, https://github.com/phetsims/geometric-optics/issues/229

--- js/common/view/VisibleProperties.ts
+++ js/common/view/VisibleProperties.ts
@@ -11,14 +11,34 @@ import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import geometricOptics from '../../geometricOptics.js';
+import OpticTypeEnum from '../model/OpticTypeEnum.js';
 
 class VisibleProperties {
+
+  // visibility of the two focal points
+  readonly focalPointsVisibleProperty: BooleanProperty;
+
+  // visibility of the virtual image, when present
+  readonly virtualImageVisibleProperty: BooleanProperty;
+
+  // visibility of the labels
+  readonly labelsVisibleProperty: BooleanProperty;
+
+  // visibility of the second 'point' (point of interest on object, or light source)
+  readonly secondPointVisibleProperty: BooleanProperty;
+
+  // visibility of the guides, which are irrelevant for mirror
+  readonly guidesVisibleProperty: BooleanProperty;
+
+  // visibility of the ray tracing
+  //TODO this is poorly named, it controls visibility of rays + real image + virtual image
+  readonly rayTracingVisibleProperty: BooleanProperty;
 
   /**
    * @param {OpticTypeEnum} opticType
    * @param {Object} [options]
    */
-  constructor( opticType, options ) {
+  constructor( opticType: OpticTypeEnum, options?: any ) { //TODO any
 
     options = merge( {
 
@@ -26,34 +46,27 @@ class VisibleProperties {
       tandem: Tandem.REQUIRED
     }, options );
 
-    // @public visibility of the two focal points
     this.focalPointsVisibleProperty = new BooleanProperty( true, {
       tandem: options.tandem.createTandem( 'focalPointsVisibleProperty' )
     } );
 
-    // @public visibility of the virtual image, when present
     this.virtualImageVisibleProperty = new BooleanProperty( true, {
       tandem: options.tandem.createTandem( 'virtualImageVisibleProperty' )
     } );
 
-    // @public visibility of the labels
     this.labelsVisibleProperty = new BooleanProperty( false, {
       tandem: options.tandem.createTandem( 'labelsVisibleProperty' )
     } );
 
-    // @public visibility of the second 'point' (point of interest on object, or light source)
     this.secondPointVisibleProperty = new BooleanProperty( false, {
       tandem: options.tandem.createTandem( 'secondPointVisibleProperty' )
     } );
 
-    // @public visibility of the guides, which are irrelevant for mirror
     this.guidesVisibleProperty = new BooleanProperty( false, {
       validValues: ( opticType === 'lens' ) ? [ true, false ] : [ false ],
       tandem: options.tandem.createTandem( 'guidesVisibleProperty' )
     } );
 
-    // @public visibility of the ray tracing
-    //TODO this is poorly named, it controls visibility of rays + real image + virtual image
     this.rayTracingVisibleProperty = new BooleanProperty( true, {
       tandem: options.tandem.createTandem( 'rayTracingVisibleProperty' )
     } );
