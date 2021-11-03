fefffe9c3c46dfd5891f4aca8d4b53e9d058799e
pixelzoom <cmalley@pixelzoom.com>
2021-11-03 08:19:58 -0600

    TODO https://github.com/phetsims/chipper/issues/1139

--- js/geometricOpticsStrings.d.ts
+++ js/geometricOpticsStrings.d.ts
@@ -1,6 +1,6 @@
 // Copyright 2021, University of Colorado Boulder
 
-//TODO-TS https://github.com/phetsims/joist/issues/764 what pattern should be used here?
+//TODO-TS https://github.com/phetsims/chipper/issues/1139 what pattern should be used here?
 
 type t = {
   [ key: string ]: string,
