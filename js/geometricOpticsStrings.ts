95320c733ead7ac0b6a026954f67465706297085
Sam Reid <reids@colorado.edu>
2021-11-11 22:37:28 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

---
+++ js/geometricOpticsStrings.ts
@@ -0,0 +1,57 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import geometricOptics from './geometricOptics.js';
+
+type StringsType = {
+  'geometric-optics': {
+    'title': string
+  },
+  'screen': {
+    'intro': string,
+    'lens': string,
+    'mirror': string
+  },
+  'concaveMirror': string,
+  'concaveLens': string,
+  'convexMirror': string,
+  'convexLens': string,
+  'object': string,
+  'focalPoint': string,
+  'focalPoints': string,
+  'labels': string,
+  'radiusOfCurvature': string,
+  'indexOfRefraction': string,
+  'diameter': string,
+  'guides': string,
+  'realImage': string,
+  'virtualImage': string,
+  'none': string,
+  'marginal': string,
+  'principal': string,
+  'many': string,
+  'secondPoint': string,
+  'valueCentimetersPattern': string,
+  'pencil': string,
+  'penguin': string,
+  'planet': string,
+  'star': string,
+  'light': string,
+  'centimeters': string,
+  'rays': string,
+  'projectionScreen': string,
+  'opticalAxis': string,
+  'focalLengthControl': string,
+  'direct': string,
+  'indirect': string
+};
+
+const geometricOpticsStrings = getStringModule( 'GEOMETRIC_OPTICS' ) as StringsType;
+
+geometricOptics.register( 'geometricOpticsStrings', geometricOpticsStrings );
+
+export default geometricOpticsStrings;
