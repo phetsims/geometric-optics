1f037bdf8ac48440835937cb5a8c82882eb8c75a
phet-dev <phetadmin@gmail.com>
2022-01-05 03:20:41 -0700

    update copyright dates from daily grunt work

--- js/common/view/LightRaysNode.ts
+++ js/common/view/LightRaysNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * LightRaysNode is responsible for rendering the rays associated with the real image and virtual image.
--- js/common/view/TargetNode.ts
+++ js/common/view/TargetNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * View of the image (both real and virtual)
