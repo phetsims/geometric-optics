9e19c2fa24c8bec4680d7ab94d30abe02044deb8
pixelzoom <cmalley@pixelzoom.com>
2022-02-09 19:44:28 -0700

    TODO

--- js/common/model/FramedImage.ts
+++ js/common/model/FramedImage.ts
@@ -1,5 +1,6 @@
 // Copyright 2021-2022, University of Colorado Boulder
 
+//TODO this entire class needs to be reviewed/revised
 /**
  * FramedImage is the model of an optical image associated with a framed object.
  *
