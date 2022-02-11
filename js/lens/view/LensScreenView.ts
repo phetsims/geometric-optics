63099256767ad5c6a6bbae54a0ea44f1c7d11b3a
pixelzoom <cmalley@pixelzoom.com>
2022-02-11 10:14:43 -0700

    drag-lock button is relevant for Arrows scene, https://github.com/phetsims/geometric-optics/issues/228

--- js/lens/view/LensScreenView.ts
+++ js/lens/view/LensScreenView.ts
@@ -73,9 +73,6 @@ class LensScreenView extends GOScreenView {
       tandem: this.controlsTandem.createTandem( 'dragLockedButton' )
     } );
     this.controlsLayer.addChild( dragLockedButton );
-    model.opticalObjectChoiceProperty.link( opticalObjectChoice => {
-      dragLockedButton.enabled = !OpticalObjectChoice.isArrowObject( opticalObjectChoice );
-    } );
 
     const lightSourceSceneNode = new LightSourceSceneNode( model.lightSourceScene, this.visibleProperties,
       this.modelViewTransform, this.modelVisibleBoundsProperty, this.modelBoundsProperty, model.raysTypeProperty, {
