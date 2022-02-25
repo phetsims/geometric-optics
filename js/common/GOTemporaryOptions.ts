439ceaf8ea2f626d6442ef7c17e40b719bedf485
pixelzoom <cmalley@pixelzoom.com>
2022-02-25 14:58:32 -0700

    doc

--- js/common/GOTemporaryOptions.ts
+++ js/common/GOTemporaryOptions.ts
@@ -4,6 +4,7 @@
  * TODO https://github.com/phetsims/geometric-optics/issues/326
  * This is a collection of temporary types for options, missing from common code when this sim was implemented.
  * As common code is converted to TypeScript, these temporary types should be replaced with the real deal.
+ * The goal is to eventually delete this file.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
