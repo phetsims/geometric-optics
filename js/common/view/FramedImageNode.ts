22452aec03b2cfca45cfb8c07ce0fa5cd7a334be
pixelzoom <cmalley@pixelzoom.com>
2022-03-02 13:48:51 -0700

    add assertions to get more info, https://github.com/phetsims/geometric-optics/issues/321

--- js/common/view/FramedImageNode.ts
+++ js/common/view/FramedImageNode.ts
@@ -84,6 +84,11 @@ class FramedImageNode extends Node {
       const initialHeight = parentNode.height;
       const scaleX = ( viewBounds.width / initialWidth ) || GOConstants.MIN_SCALE; // prevent zero scale
       const scaleY = ( viewBounds.height / initialHeight ) || GOConstants.MIN_SCALE; // prevent zero scale
+
+      //TODO https://github.com/phetsims/geometric-optics/issues/321 Assertion failed: scales should be finite
+      assert && assert( isFinite( scaleX ), `scaleX is not finite: ${scaleX}, initialWidth=${initialWidth} viewBounds.width=${viewBounds.width}` );
+      assert && assert( isFinite( scaleY ), `scaleY is not finite: ${scaleY}, initialHeight=${initialWidth} viewBounds.height=${viewBounds.width}` );
+
       parentNode.scale( scaleX, scaleY );
       parentNode.translation = new Vector2( viewBounds.minX, viewBounds.minY );
     };
