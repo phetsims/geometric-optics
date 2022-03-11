6cebb28da71d1ce0e2b12947175e5b2cc66b0c5c
pixelzoom <cmalley@pixelzoom.com>
2022-03-11 10:16:21 -0700

    replace getSelfShape with an empirical mask, https://github.com/phetsims/geometric-optics/issues/361

--- js/common/view/FramedImageNode.ts
+++ js/common/view/FramedImageNode.ts
@@ -8,6 +8,7 @@
  */
 
 import Vector2 from '../../../../dot/js/Vector2.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import { Image, Node, Path } from '../../../../scenery/js/imports.js';
 import geometricOptics from '../../geometricOptics.js';
@@ -23,6 +24,7 @@ import GOConstants from '../GOConstants.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import OpticalImageNode, { OpticalImageNodeOptions } from './OpticalImageNode.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import Matrix3 from '../../../../dot/js/Matrix3.js';
 
 type FramedImageNodeOptions = PickRequired<OpticalImageNodeOptions, 'tandem'>;
 
@@ -63,10 +65,23 @@ class FramedImageNode extends OpticalImageNode {
       hitTestPixels: true // See https://github.com/phetsims/geometric-optics/issues/283
     } );
 
+    // Mask for an image that looks like it's facing right, which occurs to the left of the optic.
+    const rightFacingMaskShape = new MaskShape( imageNode.width, imageNode.height );
+
+    // Mask for an image that looks like it's facing left, which occurs to the right of the optic.
+    // It's the same shape as the right-facing mask, but reflected about the y, and shifted to the right.
+    const scaleX = -1;
+    const translateX = imageNode.width;
+    const matrix = new Matrix3().rowMajor(
+      scaleX, 0, translateX,
+      0, 1, 0,
+      0, 0, 1
+    );
+    const leftFacingMaskShape = rightFacingMaskShape.transformed( matrix );
+
     // This mask is used to reduce the opacity of the parts of the optical axis and rays that are occluded by
-    // the real or virtual image. The shape of the mask matches the shape of imageNode, using getSelfShape.
-    // See https://github.com/phetsims/geometric-optics/issues/283.
-    const maskNode = new Path( null, {
+    // the real or virtual image. See https://github.com/phetsims/geometric-optics/issues/283.
+    const maskNode = new Path( rightFacingMaskShape, {
       fill: GOColors.screenBackgroundColorProperty,
       opacity: GOQueryParameters.frameImageMaskOpacity,
       stroke: GOQueryParameters.debugMask ? 'red' : null
@@ -106,9 +121,50 @@ class FramedImageNode extends OpticalImageNode {
     // Update the image and mask.
     framedImage.htmlImageElementProperty.link( htmlImageElement => {
       imageNode.image = htmlImageElement;
-      maskNode.shape = imageNode.getSelfShape();
+      maskNode.shape = ( framedImage.positionProperty.value.x < optic.positionProperty.value.x ) ?
+                       rightFacingMaskShape : leftFacingMaskShape;
       updateScaleAndPosition();
     } );
+  }
+}
+
+/**
+ * Mask for the framed image, used to reduce the opacity of the parts of the optical axis and rays that are occluded
+ * by the real or virtual image. The mask is drawn in the orientation of a right-facing image, which occurs to the
+ * left of the optic. See https://github.com/phetsims/geometric-optics/issues/283.
+ *
+ * The original approach was to use imageNode.getSelfShape to create a Shape that matched the image exactly. But
+ * getSelfShape is expensive, and caused performance issues when dragging the optical image. See
+ * https://github.com/phetsims/geometric-optics/issues/361.  So I switched to drawing a specific Shape that matches
+ * the image 'close enough'. This Shape is dependent on the .PNG files for the framed images, and also assumes that
+ * all .PNG files for frames images have the same dimensions.
+ *
+ * To visually inspect this mask, run with ?debugMask.
+ */
+class MaskShape extends Shape {
+
+  /**
+   * @param imageWidth
+   * @param imageHeight
+   */
+  constructor( imageWidth: number, imageHeight: number ) {
+
+    const xInset1 = 12;
+    const xInset2 = 10;
+    const yInset0 = 6;
+    const yInset1 = 55;
+    const yInset2 = 3;
+
+    super();
+
+    // Outline a right-facing framed image, starting at topLeft and moving clockwise.
+    this.moveTo( 0, yInset0 )
+      .lineTo( xInset2, yInset2 )
+      .lineTo( imageWidth - xInset1, yInset1 )
+      .lineTo( imageWidth - xInset1, imageHeight - yInset1 )
+      .lineTo( xInset2, imageHeight - yInset2 )
+      .lineTo( 0, imageHeight - yInset0 )
+      .close();
   }
 }
 
