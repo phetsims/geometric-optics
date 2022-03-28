6f93a6b14d7b34e23acabd918dcca0728192c630
pixelzoom <cmalley@pixelzoom.com>
2022-03-28 14:19:22 -0600

    add to QA credits, https://github.com/phetsims/geometric-optics/issues/146

--- js/common/GOConstants.ts
+++ js/common/GOConstants.ts
@@ -25,7 +25,7 @@ const GOConstants = {
     leadDesign: 'Amy Rouinfar, Michael Dubson',
     softwareDevelopment: 'Sarah Chang, Chris Malley (PixelZoom, Inc.), Martin Veillette',
     team: 'Chris Klusendorf, Diana L\u00f3pez Tavares, Ariel Paul, Kathy Perkins',
-    qualityAssurance: 'Steele Dalton, Emily Miller, Nancy Salpepi, Kathryn Woessner',
+    qualityAssurance: 'Steele Dalton, Clifford Hardin, Emily Miller, Nancy Salpepi, Kathryn Woessner',
     graphicArts: 'Megan Lai'
   },
 
