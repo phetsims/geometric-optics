2091f6f8f94cbc44884545bf78bb2f9f502bb2e5
phet-dev <phetadmin@gmail.com>
2022-04-07 03:32:05 -0600

    update copyright dates from daily grunt work

--- js/common/view/OriginNode.ts
+++ js/common/view/OriginNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * OriginNode is a debugging Node used to show where the origin is of something in the view, by drawing a small red dot.
