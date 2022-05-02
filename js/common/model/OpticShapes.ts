e7f99ebdf2214ffb5ade1a5eef191604a9caff90
pixelzoom <cmalley@pixelzoom.com>
2022-05-02 09:21:12 -1000

    public modifier cannot be used on interface members, https://github.com/phetsims/geometric-optics/issues/402

--- js/common/model/OpticShapes.ts
+++ js/common/model/OpticShapes.ts
@@ -13,12 +13,10 @@ import { Shape } from '../../../../kite/js/imports.js';
 interface OpticShapes {
 
   // Shapes used for ray hit testing
-  //REVIEW: Guidelines note that `public` use should be consistent within a repo. Add `public` here?
   readonly frontShape: Shape; // front (left-facing) surface, first to be hit by rays
   readonly backShape: Shape | null; // null if there is no ray hit testing on the back
 
   // Shape that defines the bounds of the optically-active part of the optic
-  //REVIEW: Guidelines note that `public` use should be consistent within a repo. Add `public` here?
   readonly activeBoundsShape: Shape;
 }
 
