c6637ec7312584d7049dccf4cae8a5e3a5c02acb
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:04 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/geometricOpticsStrings.ts
+++ js/geometricOpticsStrings.ts
@@ -9,72 +9,72 @@ import geometricOptics from './geometricOptics.js';
 
 type StringsType = {
   'geometric-optics': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'lens': string,
-    'mirror': string
-  },
-  'radiusOfCurvaturePositive': string,
-  'radiusOfCurvatureNegative': string,
-  'indexOfRefraction': string,
-  'focalLengthPositive': string,
-  'focalLengthNegative': string,
-  'diameter': string,
-  'valueCentimetersPattern': string,
-  'arrow': string,
-  'pencil': string,
-  'penguin': string,
-  'star': string,
-  'light': string,
-  'centimeters': string,
-  'rays': string,
-  'focalLengthControl': string,
+    'lens': string;
+    'mirror': string;
+  };
+  'radiusOfCurvaturePositive': string;
+  'radiusOfCurvatureNegative': string;
+  'indexOfRefraction': string;
+  'focalLengthPositive': string;
+  'focalLengthNegative': string;
+  'diameter': string;
+  'valueCentimetersPattern': string;
+  'arrow': string;
+  'pencil': string;
+  'penguin': string;
+  'star': string;
+  'light': string;
+  'centimeters': string;
+  'rays': string;
+  'focalLengthControl': string;
   'radioButton': {
-    'direct': string,
-    'indirect': string,
-    'none': string,
-    'marginal': string,
-    'principal': string,
-    'many': string
-  },
+    'direct': string;
+    'indirect': string;
+    'none': string;
+    'marginal': string;
+    'principal': string;
+    'many': string;
+  };
   'checkbox': {
-    'focalPoints': string,
-    'twoFPoints': string,
-    'labels': string,
-    'virtualImage': string,
-    'secondPoint': string,
-    'guides': string,
-    'add2FPointsCheckbox': string
-  },
+    'focalPoints': string;
+    'twoFPoints': string;
+    'labels': string;
+    'virtualImage': string;
+    'secondPoint': string;
+    'guides': string;
+    'add2FPointsCheckbox': string;
+  };
   'label': {
-    'convexLens': string,
-    'concaveLens': string,
-    'convexMirror': string,
-    'concaveMirror': string,
-    'flatMirror': string,
-    'F': string,
-    'twoF': string,
-    'object': string,
-    'objectN': string,
-    'realImage': string,
-    'realImageN': string,
-    'virtualImage': string,
-    'virtualImageN': string,
-    'projectionScreen': string,
-    'opticalAxis': string
-  },
+    'convexLens': string;
+    'concaveLens': string;
+    'convexMirror': string;
+    'concaveMirror': string;
+    'flatMirror': string;
+    'F': string;
+    'twoF': string;
+    'object': string;
+    'objectN': string;
+    'realImage': string;
+    'realImageN': string;
+    'virtualImage': string;
+    'virtualImageN': string;
+    'projectionScreen': string;
+    'opticalAxis': string;
+  };
   'keyboardHelpDialog': {
-    'moveDraggableItems': string,
-    'move': string,
-    'moveSlower': string,
-    'rulerAndMarkerControls': string,
-    'removeFromToolbox': string,
-    'returnToToolbox': string,
-    'jumpToPoint': string,
-    'chooseAnObject': string,
-    'objects': string,
-    'object': string
+    'moveDraggableItems': string;
+    'move': string;
+    'moveSlower': string;
+    'rulerAndMarkerControls': string;
+    'removeFromToolbox': string;
+    'returnToToolbox': string;
+    'jumpToPoint': string;
+    'chooseAnObject': string;
+    'objects': string;
+    'object': string;
   }
 };
 
