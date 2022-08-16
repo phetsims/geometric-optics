ebb54b7db54dcbcd7d3aeb3fde6dafc69b7c0e91
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/geometricOpticsStrings.ts
+++ js/geometricOpticsStrings.ts
@@ -5,76 +5,133 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import geometricOptics from './geometricOptics.js';
 
 type StringsType = {
   'geometric-optics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'lens': string;
+    'lensProperty': TReadOnlyProperty<string>;
     'mirror': string;
+    'mirrorProperty': TReadOnlyProperty<string>;
   };
   'radiusOfCurvaturePositive': string;
+  'radiusOfCurvaturePositiveProperty': TReadOnlyProperty<string>;
   'radiusOfCurvatureNegative': string;
+  'radiusOfCurvatureNegativeProperty': TReadOnlyProperty<string>;
   'indexOfRefraction': string;
+  'indexOfRefractionProperty': TReadOnlyProperty<string>;
   'focalLengthPositive': string;
+  'focalLengthPositiveProperty': TReadOnlyProperty<string>;
   'focalLengthNegative': string;
+  'focalLengthNegativeProperty': TReadOnlyProperty<string>;
   'diameter': string;
+  'diameterProperty': TReadOnlyProperty<string>;
   'valueCentimetersPattern': string;
+  'valueCentimetersPatternProperty': TReadOnlyProperty<string>;
   'arrow': string;
+  'arrowProperty': TReadOnlyProperty<string>;
   'pencil': string;
+  'pencilProperty': TReadOnlyProperty<string>;
   'penguin': string;
+  'penguinProperty': TReadOnlyProperty<string>;
   'star': string;
+  'starProperty': TReadOnlyProperty<string>;
   'light': string;
+  'lightProperty': TReadOnlyProperty<string>;
   'centimeters': string;
+  'centimetersProperty': TReadOnlyProperty<string>;
   'rays': string;
+  'raysProperty': TReadOnlyProperty<string>;
   'focalLengthControl': string;
+  'focalLengthControlProperty': TReadOnlyProperty<string>;
   'radioButton': {
     'direct': string;
+    'directProperty': TReadOnlyProperty<string>;
     'indirect': string;
+    'indirectProperty': TReadOnlyProperty<string>;
     'none': string;
+    'noneProperty': TReadOnlyProperty<string>;
     'marginal': string;
+    'marginalProperty': TReadOnlyProperty<string>;
     'principal': string;
+    'principalProperty': TReadOnlyProperty<string>;
     'many': string;
+    'manyProperty': TReadOnlyProperty<string>;
   };
   'checkbox': {
     'focalPoints': string;
+    'focalPointsProperty': TReadOnlyProperty<string>;
     'twoFPoints': string;
+    'twoFPointsProperty': TReadOnlyProperty<string>;
     'labels': string;
+    'labelsProperty': TReadOnlyProperty<string>;
     'virtualImage': string;
+    'virtualImageProperty': TReadOnlyProperty<string>;
     'secondPoint': string;
+    'secondPointProperty': TReadOnlyProperty<string>;
     'guides': string;
+    'guidesProperty': TReadOnlyProperty<string>;
     'add2FPointsCheckbox': string;
+    'add2FPointsCheckboxProperty': TReadOnlyProperty<string>;
   };
   'label': {
     'convexLens': string;
+    'convexLensProperty': TReadOnlyProperty<string>;
     'concaveLens': string;
+    'concaveLensProperty': TReadOnlyProperty<string>;
     'convexMirror': string;
+    'convexMirrorProperty': TReadOnlyProperty<string>;
     'concaveMirror': string;
+    'concaveMirrorProperty': TReadOnlyProperty<string>;
     'flatMirror': string;
+    'flatMirrorProperty': TReadOnlyProperty<string>;
     'F': string;
+    'FProperty': TReadOnlyProperty<string>;
     'twoF': string;
+    'twoFProperty': TReadOnlyProperty<string>;
     'object': string;
+    'objectProperty': TReadOnlyProperty<string>;
     'objectN': string;
+    'objectNProperty': TReadOnlyProperty<string>;
     'realImage': string;
+    'realImageProperty': TReadOnlyProperty<string>;
     'realImageN': string;
+    'realImageNProperty': TReadOnlyProperty<string>;
     'virtualImage': string;
+    'virtualImageProperty': TReadOnlyProperty<string>;
     'virtualImageN': string;
+    'virtualImageNProperty': TReadOnlyProperty<string>;
     'projectionScreen': string;
+    'projectionScreenProperty': TReadOnlyProperty<string>;
     'opticalAxis': string;
+    'opticalAxisProperty': TReadOnlyProperty<string>;
   };
   'keyboardHelpDialog': {
     'moveDraggableItems': string;
+    'moveDraggableItemsProperty': TReadOnlyProperty<string>;
     'move': string;
+    'moveProperty': TReadOnlyProperty<string>;
     'moveSlower': string;
+    'moveSlowerProperty': TReadOnlyProperty<string>;
     'rulerAndMarkerControls': string;
+    'rulerAndMarkerControlsProperty': TReadOnlyProperty<string>;
     'removeFromToolbox': string;
+    'removeFromToolboxProperty': TReadOnlyProperty<string>;
     'returnToToolbox': string;
+    'returnToToolboxProperty': TReadOnlyProperty<string>;
     'jumpToPoint': string;
+    'jumpToPointProperty': TReadOnlyProperty<string>;
     'chooseAnObject': string;
+    'chooseAnObjectProperty': TReadOnlyProperty<string>;
     'objects': string;
+    'objectsProperty': TReadOnlyProperty<string>;
     'object': string;
+    'objectProperty': TReadOnlyProperty<string>;
   }
 };
 
