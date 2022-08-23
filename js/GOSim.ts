f39392951b04f639ad99a7daba64be0de01f3b9a
pixelzoom <cmalley@pixelzoom.com>
2022-08-23 11:40:30 -0600

    delete a stray blank line

--- js/GOSim.ts
+++ js/GOSim.ts
@@ -43,7 +43,6 @@ export default class GOSim extends Sim {
               isBasicsVersion: providedOptions.isBasicsVersion,
               tandem: tandem
             } )
-
           } ]
         }
       } )
