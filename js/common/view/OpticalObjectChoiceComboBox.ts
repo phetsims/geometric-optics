3c5cb8dc8dda0114d247a42f17636392b668f31a
phet-dev <phetadmin@gmail.com>
2023-01-13 04:12:03 -0700

    update copyright dates from daily grunt work

--- js/common/view/OpticalObjectChoiceComboBox.ts
+++ js/common/view/OpticalObjectChoiceComboBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * OpticalObjectChoiceComboBox is the combo box for selecting the object representation.
