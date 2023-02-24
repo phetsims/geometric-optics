3e9b1ca1756fd5713102aa4c2737d3864f48f601
phet-dev <phetadmin@gmail.com>
2023-02-24 04:15:42 -0700

    update copyright dates from daily grunt work

--- js/common/model/GORay.ts
+++ js/common/model/GORay.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * GORay extends phet.dot.Ray2 by adding a length property. Generally speaking, a ray has one endpoint and goes on
