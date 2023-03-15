33cb7e9048cca4ce50a4a2a4299023b277e8f99f
phet-dev <phetadmin@gmail.com>
2023-03-15 04:19:10 -0600

    update copyright dates from daily grunt work

--- js/common/view/tools/GOToolKeyboardDragListener.ts
+++ js/common/view/tools/GOToolKeyboardDragListener.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * GOToolKeyboardDragListener is the KeyboardDragListener for use with Geometric Optics tools.
