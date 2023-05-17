0a1651c6b33e4c65f7e2d46dc1a015e881c02a0c
phet-dev <phetadmin@gmail.com>
2023-05-17 03:42:36 -0600

    update copyright dates from daily grunt work

--- js/common/view/HTMLImageElementObjectNode.ts
+++ js/common/view/HTMLImageElementObjectNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * HTMLImageElementObjectNode is the view of an object that uses an HTMLImageElement for its visual representation.
