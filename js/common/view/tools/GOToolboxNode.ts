cd68a79a1dbadae539bcadb24dad84a20755e3c9
phet-dev <phetadmin@gmail.com>
2024-03-13 03:17:48 -0600

    update copyright dates from daily grunt work

--- js/common/view/tools/GOToolboxNode.ts
+++ js/common/view/tools/GOToolboxNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * GOToolboxNode is the toolbox that holds the tools - rulers and position markers.
