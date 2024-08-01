8074e06ab096fc2fc0339b7f46fca3aff27ef7f2
phet-dev <phetadmin@gmail.com>
2024-08-01 03:48:47 -0600

    update copyright dates from daily grunt work

--- js/common/view/RulerAndMarkerControlsSection.ts
+++ js/common/view/RulerAndMarkerControlsSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * RulerAndMarkerControlsSection is the keyboard-help section that describes the hotkeys related to the tools.
