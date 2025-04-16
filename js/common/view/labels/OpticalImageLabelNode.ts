6b1dda69b25bf9844e96cdc29ca611b87d150c89
phet-dev <phetadmin@gmail.com>
2025-04-16 04:40:31 -0600

    update copyright dates from daily grunt work

--- js/common/view/labels/OpticalImageLabelNode.ts
+++ js/common/view/labels/OpticalImageLabelNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * OpticalImageLabelNode is the base class of labeling optical images, and distinguishes between real and virtual.
